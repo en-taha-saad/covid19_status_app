8df328800900803457f74454b9add02562eb80ef
Taha Saad <programmer.taha.1y@gmail.com>
2021-04-07 02:59:17 +0300

    creating the api class

---
+++ lib/app/services/api.dart
@@ -0,0 +1,12 @@
+import 'api_keys.dart';
+
+class API {
+  API({this.apiKey = ''});
+  final String apiKey;
+  factory API.sandbox() => API(apiKey: APIKeys.covidSandboxKey);
+  Uri tokenUri() => Uri(
+    scheme: 'https',
+    host: 'ncov2019-admin.firebaseapp.com',
+    path: 'token',
+  );
+}
