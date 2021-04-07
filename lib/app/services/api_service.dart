226d945ce34b29427eb7ec93bf589e1c4ac815fc
Taha Saad <programmer.taha.1y@gmail.com>
2021-04-07 03:29:24 +0300

    parsing http response

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,14 +1,25 @@
+import 'dart:convert' as convert;
+
 import 'package:covid19_status_app/app/services/api.dart';
 import 'package:http/http.dart' as http;
 
 class APIService {
   APIService(this.api);
   final API api;
-  Future getAccessToken() async {
+  Future<String> getAccessToken() async {
     final response = await http.post(
       api.tokenUri(),
       headers: {'Authorization': 'Basic ${api.apiKey}'},
     );
-    
+    if (response.statusCode == 200) {
+      final data = convert.jsonDecode(response.body);
+      final accessToken = data['access_token'];
+      if (accessToken != null) {
+        return accessToken;
+      }
+    }
+    print(
+        'Request ${api.apiKey} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}',);
+    throw response;
   }
 }
