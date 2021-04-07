181565cd11f7de3b2c865ec455a876ffe4cc3603
Taha Saad <programmer.taha.1y@gmail.com>
2021-04-07 10:14:55 +0300

    adding other endpoints to the api

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,12 +1,29 @@
 import 'api_keys.dart';
 
+enum Endpoints { cases, casesSuspected, casesConfirmed, deaths, recovered }
+
 class API {
   API({this.apiKey = ''});
   final String apiKey;
   factory API.sandbox() => API(apiKey: APIKeys.covidSandboxKey);
+  String host = 'ncov2019-admin.firebaseapp.com';
   Uri tokenUri() => Uri(
         scheme: 'https',
-        host: 'ncov2019-admin.firebaseapp.com',
+        host: host,
         path: 'token',
       );
+
+  Uri endpointsUri(Endpoints endpoint) => Uri(
+        scheme: 'https',
+        host: host,
+        path: _paths[endpoint],
+      );
+
+  Map<Endpoints, String> _paths = {
+    Endpoints.cases: "cases",
+    Endpoints.casesConfirmed: "casesConfirmed",
+    Endpoints.deaths: "deaths",
+    Endpoints.recovered: "recovered",
+    Endpoints.casesSuspected: "casesSuspected"
+  };
 }
